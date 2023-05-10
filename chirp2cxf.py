97e4b03ff0e045677e43b6deef7394d014b86ccf
G1LRO <133013430+G1LRO@users.noreply.github.com>
2023-05-10 10:40:56 +0100

    Update chirp2cxf.py

--- chirp2cxf.py
+++ chirp2cxf.py
@@ -1,3 +1,19 @@
+#Copyright 2023 G1LRO
+
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+
+
 import csv
 import sys
 
@@ -10,6 +26,7 @@ except IndexError:
     # Handle missing arguments
     print("Usage: chirp2cxf.exe chirpfile.csv quanshengfile.cxf")
     sys.exit(1)    
+
 
 #chirpfile = 'chirp.csv'
 #cxffile = 'que.cxf'
@@ -159,9 +176,7 @@ with open(chirpfile, mode='r') as csv_file:
             Bandwidth = 1
         
         
-        # Do something with the variables
-        #print(Location, " ", Name, " ", Rxfreq," ", Duplex, " ",Offset, " ",Txfreq, "rToneFreq =", AnaTxCTCIndex)
-
+        
         #Lets print the XML
         
         #print ('    <Channel Name=\"'+Name+'\" chanIndex=\"'+str(chanIndex)+'\">')
